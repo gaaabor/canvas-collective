9d480d00d448001e3ff5d8f5c45b3fafe5976764
Gabor Bencsik <gaabor.bencsik@gmail.com>
2021-08-08 22:24:55 +0200

    sync tsconfig paths with webpack aliases

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,11 +6,12 @@ exports.onCreateWebpackConfig = function ({ actions }) {
       alias: {
         '@src': path.resolve(__dirname, 'src'),
         '@components': path.resolve(__dirname, 'src/components'),
+        '@constants': path.resolve(__dirname, 'src/constants'),
         '@grid': path.resolve(__dirname, 'src/components/grid'),
         '@hooks': path.resolve(__dirname, 'src/hooks'),
         '@styles': path.resolve(__dirname, 'src/styles'),
         '@utilities': path.resolve(__dirname, 'src/utilities'),
-        '@types': path.resolve(__dirname, 'src/types.ts'),
+        '@types': path.resolve(__dirname, 'src/types'),
         '@assets': path.resolve(__dirname, 'static/assets'),
       },
     },
